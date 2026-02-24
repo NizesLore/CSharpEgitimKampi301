7ac929f66dcd0497515fd6c1d79e502765059120
Sezin EROL <fenciszn@hotmail.com>
2026-02-24 13:23:19 +0300

    .

--- CSharpEgitimKampi301.EntityLayer/Concrete/Category.cs
+++ CSharpEgitimKampi301.EntityLayer/Concrete/Category.cs
@@ -8,6 +8,9 @@ namespace CSharpEgitimKampi301.EntityLayer.Concrete
 {
     public class Category
     {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public bool CategoryStatus { get; set; }
 
     }
 }
