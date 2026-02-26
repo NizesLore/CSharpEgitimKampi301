d0776146af053cf5cd5121f506e59d16b1439467
Sezin EROL <fenciszn@hotmail.com>
2026-02-26 21:41:54 +0300

    .

--- CSharpEgitimKampi301.EntityLayer/Concrete/Product.cs
+++ CSharpEgitimKampi301.EntityLayer/Concrete/Product.cs
@@ -10,7 +10,7 @@ namespace CSharpEgitimKampi301.EntityLayer.Concrete
     {
         public int ProductId { get; set; }
         public string ProductName { get; set; }
-        public int ProductStock { get; set; }
+        public int ProductStock { get; set; }  
         public decimal ProductPrice { get; set; }  
         public string ProductDescription { get; set; }  
     }
