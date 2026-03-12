4f8abc26e56316dc0e43268a42224c3804e6bf83
Sezin EROL <fenciszn@hotmail.com>
2026-03-12 23:09:07 +0300

    .

--- CSharpEgitimKampi301.PresentationLayer/Program.cs
+++ CSharpEgitimKampi301.PresentationLayer/Program.cs
@@ -7,7 +7,7 @@ using System.Windows.Forms;
 namespace CSharpEgitimKampi301.PresentationLayer
 {
     internal static class Program
-    {
+    { 
         /// <summary> 
         /// The main entry point for the application.       
         /// </summary>  
