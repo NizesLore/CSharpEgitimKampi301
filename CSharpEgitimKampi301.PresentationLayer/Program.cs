39ac4be11fdf742e606af57010746c35a5240b75
Sezin EROL <fenciszn@hotmail.com>
2026-03-16 22:08:47 +0300

    .

--- CSharpEgitimKampi301.PresentationLayer/Program.cs
+++ CSharpEgitimKampi301.PresentationLayer/Program.cs
@@ -7,12 +7,12 @@ using System.Windows.Forms;
 namespace CSharpEgitimKampi301.PresentationLayer
 {
     internal static class Program
-    { 
+    {  
         /// <summary> 
         /// The main entry point for the application.       
         /// </summary>  
         [STAThread]  
-        static void Main()            
+        static void Main()             
         { 
             Application.EnableVisualStyles(); 
             Application.SetCompatibleTextRenderingDefault(false); 
