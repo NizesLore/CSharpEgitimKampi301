b4e59fd41b0ce36a8077f531f55756d2602f4be3
Sezin EROL <fenciszn@hotmail.com>
2026-04-07 21:32:21 +0300

    .

--- CSharpEgitimKampi301.PresentationLayer/Program.cs
+++ CSharpEgitimKampi301.PresentationLayer/Program.cs
@@ -13,7 +13,7 @@ namespace CSharpEgitimKampi301.PresentationLayer
         /// </summary>    
         [STAThread]      
         static void Main()                               
-        {   
+        {     
             Application.EnableVisualStyles();     
             Application.SetCompatibleTextRenderingDefault(false);  
             Application.Run(new Form1());                            
