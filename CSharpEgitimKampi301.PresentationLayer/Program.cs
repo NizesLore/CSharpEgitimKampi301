407bf040e8bd19aafb0131ead82d968a9279800e
Sezin EROL <fenciszn@hotmail.com>
2026-04-17 21:56:58 +0300

    .

--- CSharpEgitimKampi301.PresentationLayer/Program.cs
+++ CSharpEgitimKampi301.PresentationLayer/Program.cs
@@ -17,6 +17,5 @@ namespace CSharpEgitimKampi301.PresentationLayer
             Application.EnableVisualStyles();     
             Application.SetCompatibleTextRenderingDefault(false);  
             Application.Run(new Form1());                               
-        }                           
-    }     
+        }                                }     
 }       
