547ff1e8f94ef20bdedd31f2d1075404765e4282
Sezin EROL <fenciszn@hotmail.com>
2026-04-21 18:07:35 +0300

    .

--- CSharpEgitimKampi301.PresentationLayer/Program.cs
+++ CSharpEgitimKampi301.PresentationLayer/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace CSharpEgitimKampi301.PresentationLayer
+namespace CSharpEgitimKampi301.PresentationLayer 
 {
     internal static class Program     
     {  
@@ -16,7 +16,7 @@ namespace CSharpEgitimKampi301.PresentationLayer
         {     
             Application.EnableVisualStyles();       
             Application.SetCompatibleTextRenderingDefault(false);  
-            Application.Run(new Form1());                                
+            Application.Run(new Form1());                                 
         }     
     }      
 }       
