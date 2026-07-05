59475240afa6355e829789dc4e92777d14ddbe47
Sezin EROL <fenciszn@hotmail.com>
2026-07-05 21:06:36 +0300

    .

--- CSharpEgitimKampi301.DataAccessLayer/Context/CampContext.cs
+++ CSharpEgitimKampi301.DataAccessLayer/Context/CampContext.cs
@@ -10,7 +10,7 @@ namespace CSharpEgitimKampi301.DataAccessLayer.Context
 {
     public class CampContext : DbContext                                                          
     {        
-        public DbSet<Category>  Categories { get; set; }                                             
+        public DbSet<Category>  Categories { get; set; }                                              
         public DbSet<Product> Products { get; set; }                          
         public DbSet<Order> Orders { get; set; }              
         public DbSet<Customer> Customers { get; set; }                                                 
