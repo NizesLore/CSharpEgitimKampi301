a0a458bb59f52e833f040d66ea6e0fdd567d0fde
Sezin EROL <fenciszn@hotmail.com>
2026-08-10 14:03:13 +0300

    .

--- CSharpEgitimKampi301.EFProject/Guide.cs
+++ CSharpEgitimKampi301.EFProject/Guide.cs
@@ -8,7 +8,7 @@
 //------------------------------------------------------------------------------
 
 namespace CSharpEgitimKampi301.EFProject
-{
+{ 
     using System; 
     using System.Collections.Generic;       
      
