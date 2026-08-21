00b98582d9d36c978df38e4576e5c4fd8cefc81f
Sezin EROL <fenciszn@hotmail.com>
2026-08-21 10:09:06 +0300

    .

--- CSharpEgitimKampi301.EFProject/Guide.cs
+++ CSharpEgitimKampi301.EFProject/Guide.cs
@@ -9,9 +9,9 @@
 
 namespace CSharpEgitimKampi301.EFProject  
 { 
-    using System; 
+    using System;  
     using System.Collections.Generic;                        
-      
+       
     public partial class Guide      
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
