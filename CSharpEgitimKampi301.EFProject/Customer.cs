897257c8c7dbd5b1eef51fdea883d4101dfd5ea4
Sezin EROL <fenciszn@hotmail.com>
2026-08-23 13:02:11 +0300

    .

--- CSharpEgitimKampi301.EFProject/Customer.cs
+++ CSharpEgitimKampi301.EFProject/Customer.cs
@@ -15,7 +15,7 @@ namespace CSharpEgitimKampi301.EFProject
     public partial class Customer   
     {
         public int CustomerId { get; set; }
-        public string CustomerName { get; set; }
+        public string CustomerName { get; set; }  
         public string CustomerSurname { get; set; }
     }
 }
